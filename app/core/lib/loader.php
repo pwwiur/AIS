f3f6610e80937a18b3d5390f47ef601bd36411d6
Amir Forsati <pwwiur@yahoo.com>
2024-10-31 18:10:52 +0330

    Fixed composer issues

--- app/core/lib/loader.php
+++ app/core/lib/loader.php
@@ -6,6 +6,9 @@ $active_libraries = [
     'time',
 ];
 
+if (file_exists(COMPOSER . 'autoload.php')) {
+    require_once COMPOSER . 'autoload.php';
+}
 
 foreach ($active_libraries as $library) {
     require_once __DIR__ . '/' . $library . '.php';
